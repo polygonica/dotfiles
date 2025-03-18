b373b23a3d295d1e7404d1d2bd98de2ba46748af
Matthew Arnold <matthewarnold@g2rc4r0hlg.tail37ff0.ts.net>
2025-03-18 12:58:17 -0700

    added basic NeoVim config

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,16 @@
+-- ~/.config/nvim/init.lua
+
+-- Basic settings
+vim.opt.number = true          -- Show line numbers
+vim.opt.relativenumber = true  -- Show relative line numbers
+vim.opt.mouse = 'a'           -- Enable mouse support
+vim.opt.ignorecase = true     -- Case insensitive search
+vim.opt.smartcase = true      -- But case sensitive when uppercase present
+vim.opt.hlsearch = true       -- Highlight search results
+vim.opt.wrap = false          -- Don't wrap lines
+vim.opt.breakindent = true    -- Preserve indentation in wrapped text
+vim.opt.tabstop = 4           -- Tab width
+vim.opt.shiftwidth = 4        -- Indentation width
+vim.opt.expandtab = true      -- Use spaces instead of tabs
+vim.opt.termguicolors = true  -- Enable 24-bit RGB color
+
